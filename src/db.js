358f1aa81782221c6c3197939ae0fa9c9c78f7c1
Rai5559 <icexbee1@gmail.com>
2023-06-04 18:58:11 -0600

    new db

--- src/db.js
+++ src/db.js
@@ -2,18 +2,22 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { PGDATABASE, PGHOST, PGPASSWORD, PGPORT, PGUSER } = process.env;
+const { POSTGRES_URL } = process.env;
 
-const sequelize = new Sequelize(
-  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-    define: {
-      timestamps: false,
+const sequelize = new Sequelize(`${POSTGRES_URL}`, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  define: {
+    timestamps: false,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+      sslmode: "require",
     },
-  }
-);
+  },
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
